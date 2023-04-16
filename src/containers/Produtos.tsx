635c1ffbe5a1018242ac5e52926d8889886cc8d9
seu-usersaneme <seu-email>
2023-04-16 07:52:18 -0300

    alterado o gerenciamento do projeto, que estáva como useState para Redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,38 +1,32 @@
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
+import { useSelector } from 'react-redux'
 
 import * as S from './styles'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+  const favoritos = useSelector(
+    (state: { favoritar: { itens: { id: number }[] } }) => state.favoritar.itens
+  )
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+  if (isLoading) return <h2>Carregando...</h2>
+
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
-    const IdsDosFavoritos = favoritos.map((f) => f.id)
-
+    const IdsDosFavoritos = favoritos.map((f: { id: number }) => f.id)
     return IdsDosFavoritos.includes(produtoId)
   }
 
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto, index) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
-            key={produto.id}
+            key={`${produto.id}-${index}`}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
